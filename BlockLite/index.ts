0e4a88e9680c73a3f2db728a42e558752daff810
Min Thike Kyaw <minthike.mtk.710@gmail.com>
2025-07-02 12:08:40 +0630

    Implementing with data is finished

--- BlockLite/index.ts
+++ BlockLite/index.ts
@@ -1,3 +1,5 @@
+import * as crypto from 'crypto';
+
 type BlockType = {
     index: number;
     prevHash: string;
@@ -8,7 +10,7 @@ type BlockType = {
 }
 const Block = {  }
 const BlockChain : BlockType[]= []
-const createBlock = (index: number, prevHash: string, hash: string, data: string, nonce: number): BlockType => {
+const createBlock = (index: number, prevHash: string, hash: string,timestamp: number, data: string, nonce: number): BlockType => {
     return {
         index,
         prevHash,
@@ -22,12 +24,26 @@ const addBlock = (data: string): void => {
     const length = BlockChain.length;
     const prevBlock = BlockChain[length - 1];
     const prevHash = prevBlock ? prevBlock.hash : '0';
-    const nonce = Math.floor(Math.random() * 1000000); // Simple nonce for demonstration
-    const hash = calculateHash(length, prevBlock.hash, Date.now(), data, nonce);
-    const newBlock = createBlock(length, prevHash, hash, data, nonce);
+    const timestamp = Date.now();
+    const {nonce, hash} = findNonce(length, prevBlock.hash, timestamp, data) // Simple nonce for demonstration
+
+    const newBlock = createBlock(length, prevHash, hash, timestamp,data, nonce);
     BlockChain.push(newBlock);
 }
 
 const calculateHash = (index: number, prevHash: string, timestamp: number, data: string, nonce: number): string => {
-    return `${index}${prevHash}${timestamp}${data}${nonce}`; // Simple hash for demonstration
+    return crypto.createHash('sha256').update(`${index}${prevHash}${timestamp}${data}${nonce}`).digest('hex');
+}
+
+const findNonce = (index: number, prevHash: string, timestamp : number, data: string, difficulty: number = 2, maxAttempt: number = 100): {nonce : number, hash: string} => {
+    let nonce = 0;
+    const prefix = '0'.repeat(difficulty);
+    while (maxAttempt--) {
+        const hash = calculateHash(index, prevHash, timestamp, data, nonce);
+        if (hash.startsWith(prefix)) {
+            return { nonce, hash };
+        }
+        nonce++;
+    }
+    throw new Error('Nonce not found within max attempts');
 }
